4724854002dd153c3489db38579d7148ed29f9e3
Marcelo Silveira <Silveira.ti@gmail.com>
2016-11-28 02:27:49 -0200

    Devel

---
+++ os/src/cmddefs.h
@@ -0,0 +1,19 @@
+#define WRITE_CONFIG 8000
+#define READ_CONFIG 8001
+#define READ_DHT 8002
+#define READ_BMP180 8003
+#define ADD_COMMAND 8004
+#define WRITE_DATA 8005
+#define READ_TIME 8006
+#define READ_OTT 8007
+
+
+
+
+
+
+/***********************OUTROS DEFINES**********************************
+ * 
+ */
+#define PLUVRESET 5000
+#define PLUVINC   5001
